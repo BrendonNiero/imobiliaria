4c486b4f757751a392fabf88d16372860edc0a36
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-08-21 10:58:05 -0300

    start frontend project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+    <main className="h-screen w-full flex items-center justify-center bg-slate-900">
+      <h1 className="text-white font-bold text-6xl">Home Page</h1>
+    </main>
+  )
+}
